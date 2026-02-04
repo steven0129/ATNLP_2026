24d3367fb130a2fe27b9b5aef824f3a75d89dd81
steven0129 <h3584935@gmail.com>
2026-02-04 17:19:33 +0000

    Adjust temperature as 0.0

--- 2026/coursework/partI/utils.py
+++ 2026/coursework/partI/utils.py
@@ -43,7 +43,7 @@ def predict_gpt(openai, messages):
     response = openai.chat.completions.create(
         model="gpt-4o-mini",
         messages=messages,
-        temperature=0.7,
+        temperature=0.0,
         max_tokens=200
     )
 
