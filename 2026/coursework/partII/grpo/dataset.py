82d9783d24b1cd60d08714631dd6c32c047ecbbf
steven0129 <h3584935@gmail.com>
2026-02-23 06:09:59 +0000

    Remove redundant sentence for system prompt

--- 2026/coursework/partII/grpo/dataset.py
+++ 2026/coursework/partII/grpo/dataset.py
@@ -8,7 +8,6 @@ def build_rl_dataset(example):
         "Solutions primarily involve performing a sequence of elementary calculations using basic arithmetic operations (+ − ×÷) to reach the final answer. "
         "A bright middle school student should be able to solve every problem: from the paper, 'Problems require no concepts beyond the level of early Algebra, and the vast majority of problems can be solved without explicitly defining a variable.' "
         "Solutions are provided in natural language, as opposed to pure math expressions."
-        "Please provide the Python Code between <code> and </code> tags to indicate the code part of the solution. The code should be executable and should not contain any errors. The code should print a number as final answer."
     )
 
     truth = example['answer'].split("####")[1].strip()
